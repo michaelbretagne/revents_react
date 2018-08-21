135209f361a9395857e264d13018f897fc7d6c51
Michael <michael.bretagne@gmail.com>
2018-08-20 20:58:20 -0700

    Add lifecycle method to update form when clicking on view event

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -8,7 +8,7 @@ const eventsDashboard = [
   {
     id: "1",
     title: "Trip to Tower of London",
-    date: "2018-03-27T11:00:00+00:00",
+    date: "2018-03-27",
     category: "culture",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
@@ -32,7 +32,7 @@ const eventsDashboard = [
   {
     id: "2",
     title: "Trip to Punch and Judy Pub",
-    date: "2018-03-28T14:00:00+00:00",
+    date: "2018-03-28",
     category: "drinks",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
@@ -59,18 +59,22 @@ class EventDashboard extends Component {
   state = {
     events: eventsDashboard,
     isOpen: false,
+    selectedEvent: null,
   };
 
   handleFormOpen = () => {
-    if (this.state.isOpen) {
-      this.setState({ isOpen: false });
-    } else {
-      this.setState({ isOpen: true });
-    }
+    this.setState({ selectedEvent: null, isOpen: true });
   };
 
   handleCancel = () => {
     this.setState({ isOpen: false });
+  };
+
+  handleEditEvent = eventToUpdate => () => {
+    this.setState({
+      selectedEvent: eventToUpdate,
+      isOpen: true,
+    });
   };
 
   handleCreateEvent = newEvent => {
@@ -84,10 +88,11 @@ class EventDashboard extends Component {
   };
 
   render() {
+    const { events, selectedEvent } = this.state;
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList events={this.state.events} />
+          <EventList onEventEdit={this.handleEditEvent} events={events} />
         </Grid.Column>
         <Grid.Column width={6}>
           <Button
@@ -97,6 +102,7 @@ class EventDashboard extends Component {
           />
           {this.state.isOpen && (
             <EventForm
+              selectedEvent={selectedEvent}
               createEvent={this.handleCreateEvent}
               handleCancel={this.handleCancel}
             />
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 
+const emptyEvent = {
+  title: "",
+  date: "",
+  city: "",
+  venue: "",
+  hostedBy: "",
+};
 class EventForm extends Component {
   state = {
-    event: {
-      title: "",
-      date: "",
-      city: "",
-      venue: "",
-      hostedBy: "",
-    },
+    event: emptyEvent,
   };
+
+  componentDidMount() {
+    if (this.props.selectedEvent !== null) {
+      this.setState({ event: this.props.selectedEvent });
+    }
+  }
+
+  static getDerivedStateFromProps({ selectedEvent }) {
+    if (selectedEvent) {
+      return {
+        event: selectedEvent,
+      };
+    } else {
+      return {
+        event: emptyEvent,
+      };
+    }
+  }
 
   onFormSubmit = evt => {
     evt.preventDefault();
