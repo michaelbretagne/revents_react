13b4bae6f6af3ebd2224747c3abe8add6f2cb371
Michael <michael.bretagne@gmail.com>
2018-09-08 21:24:45 -0700

    Create auth reducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import eventReducer from "../../features/event/eventReducer";
 import modalsReducer from "../../features/modals/modalReducer";
+import authReducer from "../../features/auth/authReducer";
 
 const rootReducer = combineReducers({
   events: eventReducer,
   form: formReducer,
   modals: modalsReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
