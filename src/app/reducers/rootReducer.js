e5cf08f51fc29d67a7e89f7622ba21187e360f3d
Michael <michael.bretagne@gmail.com>
2018-09-13 12:33:47 -0700

    Add React Redux Toastr

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
+import { reducer as toastrReducer } from "react-redux-toastr";
 import eventReducer from "../../features/event/eventReducer";
 import modalsReducer from "../../features/modals/modalReducer";
 import authReducer from "../../features/auth/authReducer";
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
   modals: modalsReducer,
   auth: authReducer,
   async: asyncReducer,
+  toastr: toastrReducer,
 });
 
 export default rootReducer;
