888f1226b9070127a7c79bba8c764ebba4fe44ee
Michael <michael.bretagne@gmail.com>
2018-09-16 21:07:52 -0700

    Create register method

--- src/features/auth/authActions.jsx
+++ src/features/auth/authActions.jsx
@@ -1,5 +1,4 @@
 import { SubmissionError } from "redux-form";
-import { SIGN_OUT_USER } from "./authConstants";
 import { closeModal } from "../modals/modalActions";
 
 export const login = creds => {
@@ -18,8 +17,28 @@ export const login = creds => {
   };
 };
 
-export const logout = () => {
-  return {
-    type: SIGN_OUT_USER,
+export const registerUser = user => {
+  return async (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    try {
+      // Create the user in auth
+      let createdUser = await firebase.auth.createUserWithEmailAndPassword(
+        user.email,
+        user.password,
+      );
+      console.log(createdUser);
+      // Update the auth profile
+      await createdUser.updateProfile({ displayName: user.displayName });
+      // Create a new profile in firestore
+      let newUser = {
+        displayName: user.displayName,
+        createdAt: firestore.FieldValue.serverTimestamp(),
+      };
+      await firestore.set(`users/${createdUser.uid}`, { ...newUser });
+      dispatch(closeModal());
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -6,7 +6,6 @@ import { NavLink, Link, withRouter } from "react-router-dom";
 import SignedOutMenu from "../Menus/SignedOutMenu";
 import SignedInMenu from "../Menus/SignedInMenu";
 import { openModal } from "../../modals/modalActions";
-import { logout } from "../../auth/authActions";
 
 class NavBar extends Component {
   handleSignIn = () => {
@@ -70,7 +69,6 @@ const mapStateToProps = state => {
 
 const actions = {
   openModal,
-  logout,
 };
 
 export default withRouter(
