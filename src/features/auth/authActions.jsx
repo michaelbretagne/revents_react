fe30a47774efae203de7fb2c0349696a80869704
Michael <michael.bretagne@gmail.com>
2018-09-19 21:28:14 -0700

    Update password form and attach auth is ready to the app

--- src/features/auth/authActions.jsx
+++ src/features/auth/authActions.jsx
@@ -1,4 +1,5 @@
-import { SubmissionError } from "redux-form";
+import { SubmissionError, reset } from "redux-form";
+import { toastr } from "react-redux-toastr";
 import { closeModal } from "../modals/modalActions";
 
 export const login = creds => {
@@ -63,6 +64,22 @@ export const socialLogin = selectedProvider => {
       }
     } catch (error) {
       console.log(error);
+    }
+  };
+};
+
+export const updatePassword = creds => {
+  return async (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    const user = firebase.auth().currentUser;
+    try {
+      await user.updatePassword(creds.newPassword1);
+      await dispatch(reset("account"));
+      toastr.success("Success", "Your password has been updated");
+    } catch (error) {
+      throw new SubmissionError({
+        _error: error.message,
+      });
     }
   };
 };
--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Grid } from "semantic-ui-react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import SettingsNav from "./SettingsNav";
@@ -6,8 +7,9 @@ import BasicPage from "./BasicPage";
 import AboutPage from "./AboutPage";
 import PhotosPage from "./PhotosPage";
 import AccountPage from "./AccountPage";
+import { updatePassword } from "../../auth/authActions";
 
-const SettingsDashboard = () => {
+const SettingsDashboard = ({ updatePassword, providerId }) => {
   return (
     <Grid>
       <Grid.Column width={12}>
@@ -16,7 +18,15 @@ const SettingsDashboard = () => {
           <Route path="/settings/basic" component={BasicPage} />
           <Route path="/settings/about" component={AboutPage} />
           <Route path="/settings/photos" component={PhotosPage} />
-          <Route path="/settings/account" component={AccountPage} />
+          <Route
+            path="/settings/account"
+            render={() => (
+              <AccountPage
+                updatePassword={updatePassword}
+                providerId={providerId}
+              />
+            )}
+          />
         </Switch>
       </Grid.Column>
       <Grid.Column width={4}>
@@ -26,4 +36,17 @@ const SettingsDashboard = () => {
   );
 };
 
-export default SettingsDashboard;
+const mapStateToProps = state => {
+  return {
+    providerId: state.firebase.auth.providerData[0].providerId,
+  };
+};
+
+const actions = {
+  updatePassword,
+};
+
+export default connect(
+  mapStateToProps,
+  actions,
+)(SettingsDashboard);
