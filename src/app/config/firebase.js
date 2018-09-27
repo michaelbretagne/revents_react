6aafcfcd924fd0bbbe4bc61d6c2923f84bcfff88
Michael <michael.bretagne@gmail.com>
2018-09-26 21:38:26 -0700

    Create photo upload method

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: "",
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -1,5 +1,6 @@
 import moment from "moment";
 import { toastr } from "react-redux-toastr";
+import PhotosPage from "./Settings/PhotosPage";
 // import { isLoaded, isEmpty } from "react-redux-firebase";
 
 export const updateProfile = user => {
@@ -14,6 +15,51 @@ export const updateProfile = user => {
       toastr.success("Success", "Profile updated");
     } catch (error) {
       console.log(error);
+    }
+  };
+};
+
+export const uploadProfileImage = (file, fileName) => {
+  return async (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    const user = firebase.auth().currentUser;
+    const path = `${user.uid}/user_images`;
+    const options = {
+      name: fileName,
+    };
+
+    try {
+      // upload the file to firebase storage
+      let uploadedFile = await firebase.uploadFile(path, file, null, options);
+      // get url of image
+      let downloadURL = await uploadedFile.uploadTaskSnapshot.downloadURL;
+      // get userdoc
+      let userDoc = await firestore.get(`users/${user.uid}`);
+      // check if user has a photo, if not update profile with new image
+      if (!userDoc.data().photoURL) {
+        await firebase.updateProfile({
+          photoURL: downloadURL,
+        });
+        await user.updateProfile({
+          photoURL: downloadURL,
+        });
+      }
+      // add the new photo to photos collection
+      return await firestore.add(
+        {
+          collection: "users",
+          doc: user.uid,
+          subcollections: [{ collection: "photos" }],
+        },
+        {
+          name: fileName,
+          url: downloadURL,
+        },
+      );
+    } catch (error) {
+      console.log(error);
+      throw new Error("Problem uploading photos");
     }
   };
 };
