c02568f27063a443f501aac09db3f950f79efa65
Michael <michael.bretagne@gmail.com>
2018-10-27 12:39:11 -0700

    Add userFollowing cloud function

--- functions/index.js
+++ functions/index.js
@@ -55,8 +55,7 @@ exports.cancelActivity = functions.firestore
       return false;
     }
 
-    const activity = newActivity(
-      "cancelledEvent", updatedEvent, context.params.eventId);
+    const activity = newActivity("cancelledEvent", updatedEvent, context.params.eventId);
 
     console.log({ activity });
 
@@ -70,4 +69,33 @@ exports.cancelActivity = functions.firestore
       .catch(err => {
         return console.log("Error adding activity", err);
       });
+  });
+
+exports.userFollowing = functions.firestore
+  .document("users/{followerUid}/following/{followingUid}")
+  .onCreate((event, context) => {
+    console.log("v1");
+    const followerUid = context.params.followerUid;
+    const followingUid = context.params.followingUid;
+
+    const followerDoc = admin.firestore().collection("users").doc(followerUid);
+
+    console.log(followerDoc);
+
+    return followerDoc.get().then(doc => {
+      let userData = doc.data();
+      console.log({ userData });
+      let follower = {
+        displayName: userData.displayName,
+        photoURL: userData.photoURL || "/assets/user.png",
+        city: userData.city || "unknown city",
+      };
+      return admin
+        .firestore()
+        .collection("users")
+        .doc(followingUid)
+        .collection("followers")
+        .doc(followerUid)
+        .set(follower);
+    });
   });
