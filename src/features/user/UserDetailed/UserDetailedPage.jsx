227831334dc3a87588dd718ce32c8b9f432d77dc
Michael <michael.bretagne@gmail.com>
2018-11-08 18:01:36 -0800

    Redirect users when trying to access to an non existing user profile

--- src/features/user/UserDetailed/UserDetailedPage.jsx
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -3,6 +3,7 @@ import { Grid } from "semantic-ui-react";
 import { connect } from "react-redux";
 import { firestoreConnect, isEmpty } from "react-redux-firebase";
 import { compose } from "redux";
+import { toastr } from "react-redux-toastr";
 import UserDetailedHeader from "./UserDetailedHeader";
 import UserDetailedDescription from "./UserDetailedDescription";
 import UserDetailedSidebar from "./UserDetailedSidebar";
@@ -14,6 +15,13 @@ import { getUserEvents, followUser, unfollowUser } from "../userActions";
 
 class UserDetailedPage extends Component {
   async componentDidMount() {
+    let user = await this.props.firestore.get(
+      `users/${this.props.match.params.id}`,
+    );
+    if (!user.exists) {
+      toastr.error("Not Found", "This is not the user you are looking for");
+      this.props.history.push("/error");
+    }
     await this.props.getUserEvents(this.props.userUid);
   }
 
@@ -36,7 +44,7 @@ class UserDetailedPage extends Component {
     } = this.props;
     const isCurrentUser = auth.uid === match.params.id;
     const isFollowing = !isEmpty(following);
-    const loading = Object.values(requesting).some(a => a === true);
+    const loading = requesting[`users/${match.params.id}`];
     if (loading) return <LoadingComponent inverted={true} />;
     return (
       <Grid>
